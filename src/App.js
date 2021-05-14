ec1b0a8a3eb2b060c071dd992ef31c418733d81f
Amd2D <amdelcheikh@gmail.com>
2021-05-14 18:28:52 +0300

    small fixes

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from './components/Home'
 import Slider from './Slider'
 
-function App(){
-    return(
+function App() {
+    return (
         <Router>
             <Switch>
                 <Route exact path="/" component={Home}/>
@@ -12,5 +12,6 @@ function App(){
             </Switch>
         </Router>
     );
+}
 
 export default App;
